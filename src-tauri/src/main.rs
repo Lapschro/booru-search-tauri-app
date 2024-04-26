e0f0c62a4aa30cf0f76c6583d1f17d882735f91f
Lapschro <gui_okita@hotmail.com>
2024-04-26 20:13:42 +0100

    comment unused sql commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -68,63 +68,63 @@ fn save_configurations(configurations: String) -> () {
         .expect("Unable to write file");
 }
 
-#[tauri::command]
-fn save_post(content: String) -> bool {
-    let conn = Connection::open("test.db").expect("Unable to open database");
+// #[tauri::command]
+// fn save_post(content: String) -> bool {
+//     let conn = Connection::open("test.db").expect("Unable to open database");
 
-    println!("Saving post: {}", content);
+//     println!("Saving post: {}", content);
 
-    match conn.execute(
-        "INSERT INTO posts (content) 
-            VALUES (?1)
-            ON CONFLICT (content) DO UPDATE SET content = ?1    
-        ",
-        [content],
-    ) {
-        Ok(_) => true,
-        Err(err) => {
-            println!("Error: {}", err);
-            false
-        }
-    }
-}
+//     match conn.execute(
+//         "INSERT INTO posts (content) 
+//             VALUES (?1)
+//             ON CONFLICT (content) DO UPDATE SET content = ?1    
+//         ",
+//         [content],
+//     ) {
+//         Ok(_) => true,
+//         Err(err) => {
+//             println!("Error: {}", err);
+//             false
+//         }
+//     }
+// }
 
-#[tauri::command]
-fn get_posts() -> String {
-    let conn = Connection::open("test.db").expect("Unable to open database");
+// #[tauri::command]
+// fn get_posts() -> String {
+//     let conn = Connection::open("test.db").expect("Unable to open database");
 
-    let mut statement = conn
-        .prepare("SELECT id, content FROM posts")
-        .expect("Unable to prepare statement");
-    let posts_iter = statement
-        .query_map([], |row| {
-            Ok(Post {
-                id: row.get(0)?,
-                content: row.get(1)?,
-            })
-        })
-        .expect("Unable to query map");
+//     let mut statement = conn
+//         .prepare("SELECT id, content FROM posts")
+//         .expect("Unable to prepare statement");
+//     let posts_iter = statement
+//         .query_map([], |row| {
+//             Ok(Post {
+//                 id: row.get(0)?,
+//                 content: row.get(1)?,
+//             })
+//         })
+//         .expect("Unable to query map");
 
-    let mut res = Vec::new();
-    for post in posts_iter {
-        res.push(post.unwrap());
-    }
+//     let mut res = Vec::new();
+//     for post in posts_iter {
+//         res.push(post.unwrap());
+//     }
 
-    serde_json::to_string(res.as_slice()).expect("Unable to serialize")
-}
+//     serde_json::to_string(res.as_slice()).expect("Unable to serialize")
+// }
 
 #[tokio::main]
 async fn main() {
-    let conn = Connection::open("test.db").expect("Unable to open database");
+    // let conn = Connection::open("test.db").expect("Unable to open database");
 
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS posts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            content TEXT NOT NULL UNIQUE
-        )",
-        [],
-    )
-    .expect("Unable to create table");
+    // conn.execute(
+    //     "CREATE TABLE IF NOT EXISTS posts (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         content TEXT NOT NULL UNIQUE
+    //     )",
+    //     [],
+    // )
+    // .expect("Unable to create table");
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
@@ -132,8 +132,8 @@ async fn main() {
             download,
             get_configurations,
             save_configurations,
-            save_post,
-            get_posts,
+            // save_post,
+            // get_posts,
             fetch
         ])
         .run(tauri::generate_context!())
